4dfa30360523e7b0c3abd3fc295253a1fa58ae50
tainur <tainurrahaman31@gmail.com>
2025-02-24 17:38:42 +0600

    read review data using scholarship id and reviewer id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -23,18 +23,96 @@ const userCollection = client.db("ScholarBeaconDB").collection("users");
 const scholarshipCollection = client
   .db("ScholarBeaconDB")
   .collection("scholarships");
+const reviewCollection = client.db("ScholarBeaconDB").collection("reviews");
 
 async function run() {
   try {
+    // Users API
     app.post("/users", async (req, res) => {
       const newUser = req.body;
       const result = await userCollection.insertOne(newUser);
       res.send(result);
     });
 
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await userCollection.findOne({ email: email });
+      res.send(result);
+    });
+
+    // Scholarships API
     app.get("/scholarships", async (req, res) => {
       const result = await scholarshipCollection.find().toArray();
       res.send(result);
+    });
+
+    app.get("/scholarships/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await scholarshipCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Review DB
+
+    app.get("/reviews/:scholarship_id", async (req, res) => {
+      try {
+        const id = req.params.scholarship_id;
+        const query = { scholarship_id: id };
+
+        const reviews = await reviewCollection.find(query).toArray();
+
+        const reviewsWithUserDetails = await Promise.all(
+          reviews.map(async (review) => {
+            const user = await userCollection.findOne({
+              _id: new ObjectId(review.reviewer_id),
+            });
+            return {
+              ...review,
+              reviewer_name: user ? user.name : "Unknown",
+              reviewer_image: user ? user.image : "default.jpg",
+            };
+          })
+        );
+
+        res.json(reviewsWithUserDetails);
+      } catch (error) {
+        console.error("Error fetching reviews:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
+    app.get("/reviews/r_id/:reviewer_id", async (req, res) => {
+      try {
+        const reviewerId = req.params.reviewer_id;
+        const query = { reviewer_id: reviewerId };
+
+        const reviews = await reviewCollection.find(query).toArray();
+
+        const reviewsWithDetails = await Promise.all(
+          reviews.map(async (review) => {
+            const scholarship = await scholarshipCollection.findOne({
+              _id: new ObjectId(review.scholarship_id),
+            });
+
+            return {
+              scholarship_name: scholarship
+                ? scholarship.subject_name
+                : "Unknown",
+              university_name: scholarship
+                ? scholarship.university_name
+                : "Unknown",
+              review_comments: review.reviewer_comments,
+              review_date: review.review_date,
+            };
+          })
+        );
+
+        res.json(reviewsWithDetails);
+      } catch (error) {
+        console.error("Error fetching reviews:", error);
+        res.status(500).send("Internal Server Error");
+      }
     });
 
     // Connect the client to the server	(optional starting in v4.7)
